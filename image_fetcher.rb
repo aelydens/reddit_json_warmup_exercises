e9a488530e502ef91ea805ec86c3af814cb8c289
Annie Lydens <aelydens@gmail.com>
2015-03-16 13:17:44 -0600

    All tests pass

--- image_fetcher.rb
+++ image_fetcher.rb
@@ -7,15 +7,25 @@ class ImageFetcher
   end
 
   def count_children
+    @data["data"]["children"].count
   end
 
   def first_child
+    @data["data"]["children"].first["data"]
   end
 
   def all_images
+    @data["data"]["children"].map {|child| child["data"]["url"] }
   end
 
   # fetch only the images that begin with `http://i.imgur.com`
   def only_imgur_images
+    only_imgur = []
+    children = @data["data"]["children"].each do |child|
+      if child["data"]["url"].include?("http://i.imgur.com")
+        only_imgur << child["data"]["url"]
+      end
+    end
+    only_imgur
   end
 end
